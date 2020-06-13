731794a835b679ce1f0e01383e534552aa608872
tomery11 <tomeryona92@gmail.com>
2020-06-13 20:13:53 +0300

    Added connect button and input for url.. need to add constraints

--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.flightmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val btn_click_me = findViewById(R.id.connectButton) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }
