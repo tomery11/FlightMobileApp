8f066b1a4c17aa0fadc45d4d5291cd3d45114436
tomery11 <tomeryona92@gmail.com>
2020-06-16 11:39:36 +0300

    Working on controls for flight simualtor: -added seekbars and generic pic for now - finsihed design of dashboard activity_dashboard.xml

--- app/src/main/java/com/example/flightmobileapp/Dashboard.kt
+++ app/src/main/java/com/example/flightmobileapp/Dashboard.kt
@@ -2,10 +2,36 @@ package com.example.flightmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
-class Dashboard : AppCompatActivity() {
+class Dashboard : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+
+    private var rudderSeekbarView: SeekBar? =null
+    private var throttleSeekbarView: SeekBar? =null
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        rudderSeekbarView = this.rudderSeekbar
+        rudderSeekbarView!!.setOnSeekBarChangeListener(this)
+        throttleSeekbarView = this.throttleSeekbar
+        throttleSeekbarView!!.setOnSeekBarChangeListener(this)
+
+    }
+
+    override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartTrackingTouch(p0: SeekBar?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStopTrackingTouch(p0: SeekBar?) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.example.flightmobileapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
